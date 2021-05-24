e1ff79f14a405854c13e7f6d7e3b88ca0bae27d3
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-24 16:42:57 -0400

    using head from an array

---
+++ head.js
@@ -0,0 +1,26 @@
+// function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array
+  
+};
+
+
+// test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Tooth", "Car");
+assertEqual("car", "car");
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
