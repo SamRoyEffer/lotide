699f3e2bb643183ba6cf81c5a795544dcd6da082
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-26 13:16:14 -0400

    Added assertion to final statment

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,27 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
 const assertArraysEqual = function(array1, array2){
   let firstArray = array1;
   let secArray = array2;
-
-  for (let i = 0; i < firstArray.length; i++){
-    for (let j = 0; j < secArray.length; j++){
-      if (secArray.length[j] === firstArray.length[i]) {
-        
-        return "Excellent work";
-      } else {
-        return "That is not right";
-      }
-    }
+  if (secArray.length !== firstArray.length){
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secArray}`);
   }
+  for(let i = 0; i < firstArray.length; i++){
+    if (firstArray[i] !== secArray[i]){
+      
+      console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secArray}`);
+    } 
+  }
+  console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secArray}`);
+
 }
 
