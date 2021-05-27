521f1eb4dd2d5e4cfeb4ac42c71272bd0f4684d7
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-27 09:25:35 -0400

    Functioning function

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+//take a sting, seperate it and count the individual letters
+const countLetters = function(scentence) {
+  const results = {};
+  for (const letter of scentence) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+
+
+console.log((countLetters("Hello my name is what")));
