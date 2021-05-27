69561d0fe3e177216b4b43ebcef037bcacfe4ece
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-27 13:16:33 -0400

    Working function

---
+++ letterPositions.js
@@ -0,0 +1,19 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let position in sentence) {
+    const key = sentence[position];//letter in the sentence
+    if (key !== ' ') {
+      if (results[key]) {
+        console.log(sentence[position]);
+        results[key].push(position);
+      } else {
+        results[key] = [position];
+      }
+    }
+  }
+  return results;
+};
+
+
+console.log(letterPositions("hello"));
+console.log(letterPositions("My name is WHAT?!"));
