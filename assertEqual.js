b787a8a2d6d81956c9b3021871b0161116b2042a
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-31 12:55:14 -0400

    cleaned up the code with the module additions

--- assertEqual.js
+++ assertEqual.js
@@ -8,11 +8,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertEqual;
 
 
-
-// test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Tooth", "Car");
-assertEqual("car", "car");
+// // test code
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1,1);
+// assertEqual("Tooth", "Car");
+// assertEqual("car", "car");
--- head.js
+++ head.js
@@ -1,19 +1,16 @@
 // function implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("./assertEqual")
 
 const head = function(array) {
-  return array
+  
+  if (array === []) {
+    return undefined;
+  } else {
+    return array[0];
+  }
   
 };
 
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
