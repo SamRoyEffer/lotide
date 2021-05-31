d5aac2d3b29ba6c8a6672ba74026d902ec3dec7f
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-31 13:18:35 -0400

    module export function and test file

--- eqArrays.js
+++ eqArrays.js
@@ -11,10 +11,4 @@ const eqArrays = function(array1, array2){
   return console.log(true);
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays
