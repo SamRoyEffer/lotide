59b9087f1fb8aa197cb3a476d50366a2922a14ef
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-31 15:21:16 -0400

    cleaned up the files and implimented mocha and chai checks

--- head.js
+++ head.js
@@ -9,5 +9,5 @@ const head = function(array) {
 
 module.exports = head
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,14 @@
-const assertEqual = require('../assertEqual')
+//const assertEqual = require('../assertEqual')
 const head = require('../head');
 
+const assert = require('chai').assert;
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]))
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]),1);
+  });
+  it("return 2 for [2,4,6,8]", () => {
+    assert.strictEqual(head([2,4,6,8]),2);
+  });
+
+});
