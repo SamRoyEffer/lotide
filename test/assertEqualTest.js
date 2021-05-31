52f7681e24129d1fe0da13f66ac5207b9f3e811a
Sam Roy-Effer <samroyeffer@gmail.com>
2021-05-31 12:55:57 -0400

    additional files and assingments left over

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+
+const assertEqual = require('../assertEqual')
+
+
+// test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Tooth", "Car");
+assertEqual("car", "car");
